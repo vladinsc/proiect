511f7c40044f33a2bdc6ea2a7c689da4a6d1e64a
vladinsc <vladcristianionescu@gmail.com>
2025-01-16 14:55:17 +0200

    stergehar ta

--- locatie.js
+++ locatie.js
@@ -1,23 +1,7 @@
 window.onload = load();
 function load(){
-    document.getElementById("imprd").addEventListener("click", changeColor); 
-    function changeColor(){
-        document.getElementById("enr1").style.color = "white";
-        document.getElementById("enr2").style.color = "white";
-        document.getElementById("enr3").style.color = "white";
-        document.getElementById("enr4").style.color = "white";
-        document.getElementById("enr5").style.color = "white";
-        document.getElementById("enr6").style.color = "white";
-        document.getElementById("enr7").style.color = "white";
-        document.getElementById("enr8").style.color = "white";
-
-        document.getElementById("enr1").style.fontSize = "x-large";
-        document.getElementById("enr2").style.fontSize = "x-large";
-        document.getElementById("enr3").style.fontSize = "x-large";
-        document.getElementById("enr4").style.fontSize = "x-large";
-        document.getElementById("enr5").style.fontSize = "x-large";
-        document.getElementById("enr6").style.fontSize = "x-large";
-        document.getElementById("enr7").style.fontSize = "x-large";
-        document.getElementById("enr8").style.fontSize = "x-large";
+    document.getElementById("buttonMap").addEventListener("click", stergeHarta); 
+    function stergeHarta(){
+        document.getElementById("harta").remove();
     }
 }
